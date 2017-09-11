e3c9c1793e7787b53e6d20886bee8e49b4d38447
suntyop <mathistudico@gmail.com>
2017-09-11 22:56:04 +0200

    Add files via upload

---
+++ platform_www/plugins/jp.wizcorp.phonegap.plugin.wizAssetsPlugin/www/phonegap/plugin/wizAssets/wizAssets.js
@@ -0,0 +1,48 @@
+cordova.define("jp.wizcorp.phonegap.plugin.wizAssetsPlugin.wizAssetsPlugin", function(require, exports, module) {
+/* Download and show, PhoneGap Example
+ *
+ * @author Ally Ogilvie
+ * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
+ * @file - wizAssets.js
+ * @about - JavaScript download and update asset example for PhoneGap
+ *
+ *
+ */
+var exec = require("cordova/exec");
+var wizAssets = {
+    initialize: function (s, f) {
+        return exec(s, function (error) {
+            return f(WizAssetsError.generate(error));
+        }, "WizAssetsPlugin", "initialize", []);
+    },
+    downloadFile: function (url, filePath, s, f) {
+        return window.setTimeout(function () {
+            return exec(s, function (error) {
+                return f(WizAssetsError.generate(error));
+            }, "WizAssetsPlugin", "downloadFile", [url, filePath]);
+        }, 0);
+    },
+    deleteFile: function (uri, s, f) {
+        return exec(s, function (error) {
+            return f(WizAssetsError.generate(error));
+        }, "WizAssetsPlugin", "deleteFile", [uri]);
+    },
+    deleteFiles: function (uris, s, f) {
+        return exec(s, function (error) {
+            return f(WizAssetsError.generate(error));
+        }, "WizAssetsPlugin", "deleteFiles", uris);
+    },
+    getFileURIs: function (s, f) {
+        return exec(s, function (error) {
+            return f(WizAssetsError.generate(error));
+        }, "WizAssetsPlugin", "getFileURIs", []);
+    },
+    getFileURI: function (uri, s, f) {
+        return exec(s, function (error) {
+            return f(WizAssetsError.generate(error));
+        }, "WizAssetsPlugin", "getFileURI", [uri]);
+    }
+};
+
+module.exports = wizAssets;
+});
